2d0bcf17ea1c9ec962a03ca9ec522618877012c5
Brown <a_brown24@tss-hr420-dm19.fc.ca>
2017-02-15 13:07:54 -0500

    updated map files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 (function(){
 
-var map, marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')), marker;
 
-function initMap() {
-map = new google.maps.Map(document.querySelector('.map-wrapper'),
-		{
-			center: { lat: 42.983233, lng: -81.250688 },
-			zoom: 14
-		}
-	);
+	function initMap(position) {
 
-	marker = new google.maps.Marker({
-		position : { lat: 42.983233, lng: -81.250688 },
-		map: map,
-		title: 'Hello World!'
-	});
+		map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+
+		map.setZoom(16);
+
+		marker = new google.maps.Marker({
+			position : { lat: position.coords.latitude, lng: position.coords.longitude },
+			map: map,
+			title: 'Hello World!'
+		});
 	}
 
-	initMap();
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(initMap, handleError);
+	}
+		else {
+	console.log('Your Browser does not have geolocation');
+		}
+
+
+	function handleError(e) {
+		console.log(e);
+	}
+
 })();
