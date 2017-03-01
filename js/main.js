463f9a89aabd2bd16cf9446934bb4debc393d309
Brown <a_brown24@tss-hr420-dm19.fc.ca>
2017-03-01 13:04:25 -0500

    added geocoding and directions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 (function(){
 
-	var map = new google.maps.Map(document.querySelector('.map-wrapper')),preloader = document.querySelector('.preload-wrapper'), marker;
+	var map = new google.maps.Map(document.querySelector('.map-wrapper')),
+	preloader = document.querySelector('.preload-wrapper'),
+	geocoder = new google.maps.Geocoder(),geocodeButton = document.querySelector('.geocode'),
+	directionsService = new google.maps.DirectionsService(),
+	directionsDisplay,
+	locations = [],
+	marker;
 
 	function initMap(position) {
+		locations[0] = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		directionsDisplay.setMap(map);
+
 
 		map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
 
@@ -17,6 +28,58 @@
 		preloader.classList.add('hide-preloader');
 	}
 
+//geocoding API - find an address on the map
+function codeAddress() {
+//	debugger;
+var address = document.querySelector('.address').value;
+
+geocoder.geocode( {'address' : address}, function(results, status) {
+	if (status === google.maps.GeocoderStatus.OK) {
+
+
+		locations[1] = {lat: results[0].geometry.location.lat(),
+
+			lng: results[0].geometry.location.lng() };
+
+
+
+		map.setCenter(results[0].geometry.location);
+
+		if (marker) {
+			marker.setMap(null);
+
+			marker = new google.maps.Marker({
+				map: map,
+				position: results[0].geometry.location
+			});
+		}
+
+		calcRoute(results[0].geometry.location);
+
+	} else
+	{
+		console.log('Geocoder was not successful for the following reason: ', status);
+	}
+});
+}
+
+function calcRoute(codedLoc) {
+	var request = {
+		origin: locations[0],
+		destination: locations[1],
+		travelMode: 'DRIVING'
+	};
+	directionsService.route(request, function(response, status){
+		if (status === 'OK') {
+			directionsDisplay.setDirections(response);
+		}
+	});
+}
+
+
+
+
+
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(initMap, handleError);
 	}
@@ -28,5 +91,7 @@
 	function handleError(e) {
 		console.log(e);
 	}
+
+geocodeButton.addEventListener('click', codeAddress, false);
 
 })();
